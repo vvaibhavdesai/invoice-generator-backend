b8af95314a5713a7a9b595aff6f7a24b8f8f8112
vvaibhav <vaibhavdesai888@gmail.com>
2022-02-12 01:04:19 +0530

    import updated

--- routes/InvoiceRoute.js
+++ routes/InvoiceRoute.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { checkAuth } = require("../middlewares/checkAuth");
-const invoice = require("../controllers/Invoice");
-router.route("/all").get(checkAuth, invoice.getAll);
-router.route("/create").post(checkAuth, invoice.createInvoice);
+const { getAll, createInvoice } = require("../controllers/Invoice");
+
+router.route("/all").get(checkAuth, getAll);
+router.route("/create").post(checkAuth, createInvoice);
 module.exports = router;
